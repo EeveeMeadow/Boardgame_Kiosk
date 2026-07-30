98ddd767fcf99d1d7dbfe64e9e48eda68d0958c6
Eevee <eevee232@gmail.com>
2026-07-30 15:17:11 -0700

    added .env and README files

---
+++ README.md
@@ -0,0 +1,14 @@
+# Setup
+1. Package Installation
+```bash
+pip install -r requirements.txt
+```
+2. Setting up Environment
+   1. create .env file in root
+```dotenv
+PG_USER=
+PG_PASS=
+PG_HOST=
+PG_PORT=
+PG_DB=
+```
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from os import getenv
+
+from dotenv import load_dotenv
 from nicegui import ui, app
 from tortoise import TortoiseConfig
 from tortoise.config import DBUrlConfig, AppConfig
@@ -6,12 +9,19 @@ from tortoise.contrib.fastapi import register_tortoise
 from regions import BaseRouter
 from regions import ToolRouter
 
+load_dotenv()
+pg_user = getenv("PG_USER")
+pg_password = getenv("PG_PASS")
+pg_host = getenv("PG_HOST")
+pg_port = getenv("PG_PORT")
+pg_db = getenv("PG_DB")
+
 register_tortoise(
         app,
         config=TortoiseConfig(
             use_tz=True,
             timezone='UTC',
-            connections={'default': DBUrlConfig(url='postgres://eveya:AryaLovesEve@192.168.1.51:31854/boardgames')},
+            connections={'default': DBUrlConfig(url=f'postgres://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')},
             apps={'models': AppConfig(models=['components.models'])}
         )
     )
